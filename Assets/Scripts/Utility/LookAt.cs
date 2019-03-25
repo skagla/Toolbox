0f8fa62f9cd0dcc3432a11d38bb766c0282b2ded
skagla <johucek@gmx.at>
2019-03-25 20:40:37 +0100

    Start Sample Scenes

---
+++ Assets/Scripts/Utility/LookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public GameObject lookAtTarget;
+
+    void Update()
+    {
+        if (lookAtTarget != null)
+            transform.LookAt(lookAtTarget.transform.position); 
+    }
+}
